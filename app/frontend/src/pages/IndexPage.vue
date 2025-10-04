3488609d63a02fbf5fa3ae55e8d11edb7d9deb1a
Jajo1010 <ladislav.stefunjr@gmail.com>
2025-10-04 20:22:19 +0200

    feat(chat): change colors of chat to primary

--- app/frontend/src/pages/IndexPage.vue
+++ app/frontend/src/pages/IndexPage.vue
@@ -1,59 +1,45 @@
 <template>
   <div class="q-pa-md row justify-center">
-    <div style="width: 100%; max-width: 50%; z-index:-1;">
-      <q-chat-message
-        name="me"
-        :text="['hey, how are you?']"
-        sent
-      />
+    <div style="width: 100%; max-width: 50%; z-index: -1">
+      <q-chat-message name="me" :text="['hey, how are you?']" sent />
       <q-chat-message
         name="Jane"
+        bg-color="primary"
+        text-color="white"
         :text="['doing fine, how r you?']"
       />
-      <q-chat-message
-        name="me"
-        :text="['hey, how are you?']"
-        sent
-      />
+      <q-chat-message name="me" :text="['hey, how are you?']" sent />
       <q-chat-message
         name="Jane"
+        bg-color="primary"
+        text-color="white"
         :text="['doing fine, how r you?']"
       />
-      <q-chat-message
-        name="me"
-        :text="['hey, how are you?']"
-        sent
-      />
+      <q-chat-message name="me" :text="['hey, how are you?']" sent />
       <q-chat-message
         name="Jane"
+        bg-color="primary"
+        text-color="white"
         :text="['doing fine, how r you?']"
       />
-      <q-chat-message
-        name="me"
-        :text="['hey, how are you?']"
-        sent
-      />
-      <q-chat-message
-        name="Jane"
-        :text="['doing fine, how r you?']"
-      />
-
-
     </div>
-
   </div>
-  <div>
-  </div>
+  <div></div>
 
-  <q-page-sticky position="bottom" expand :offset="[0,0]" style="width: 50%; margin:0 auto; ">
-
-    <div class="q-pa-sm  full-width" style="max-width: 90%; background-color: grey; " >
+  <q-page-sticky
+    position="bottom"
+    expand
+    :offset="[0, 0]"
+    style="width: 50%; margin: 0 auto"
+  >
+    <div class="q-pa-sm full-width" style="max-width: 90%; background-color: grey">
       <q-input
         v-model="text"
-        filled dense
+        filled
+        dense
         placeholder="Napíš správu alebo príkaz (/join …)"
         class="full-width"
-        style="background-color: lightgray;"
+        style="background-color: lightgray"
         @keyup.enter.exact="send"
       >
         <template #append>
@@ -62,19 +48,14 @@
       </q-input>
     </div>
   </q-page-sticky>
-
-
-
-
-
 </template>
 <script setup lang="ts">
-import { ref } from 'vue'
-const text = ref('')
-function send () {
-  if (!text.value.trim()) return
+import { ref } from "vue";
+const text = ref("");
+function send() {
+  if (!text.value.trim()) return;
   // TODO: dispatch do store/command parser
-  console.log('SEND:', text.value)
-  text.value = ''
+  console.log("SEND:", text.value);
+  text.value = "";
 }
 </script>
