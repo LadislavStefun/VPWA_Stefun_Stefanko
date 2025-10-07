eff2e0149d47c065a9b62e99364814dd797bafdb
MartinStefanko1 <xstefanko@stuba.sk>
2025-10-07 19:21:37 +0200

    feat: Added typing simulation

--- app/frontend/src/pages/IndexPage.vue
+++ app/frontend/src/pages/IndexPage.vue
@@ -4,12 +4,15 @@
       <div v-for="(item, index) in items" :key="index" class="caption" >
         <div >
       <q-chat-message name="me" :text="['hey, how are you?']" sent />
-      <q-chat-message
-        name="Jane"
-        bg-color="primary"
-        text-color="white"
-        :text="['doing fine, how r you?']"
-      />
+      <q-chat-message name="Jane" bg-color="grey-4" text-color="dark">
+      <div class="row items-center no-wrap">
+      <div class="">Typing </div>
+      <q-spinner-dots size="20px" class="q-ml-xs" />
+      </div>
+      </q-chat-message>
+
+      <q-chat-message name="Jane" bg-color="primary" text-color="white" :text="['doing fine, how r you?']"/>
+
     </div>
       </div>
       <template v-slot:loading>
