7bbd1f518272c39a8209accb518236640de973ed
Jajo1010 <ladislav.stefunjr@gmail.com>
2025-10-08 23:45:49 +0200

    fix(components): rewrite logo to reusable component

---
+++ app/frontend/src/components/AppLogo.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-img :src="src" class="responsive-img q-mx-auto" fit="contain" />
+</template>
+
+<script setup lang="ts">
+import logoImage from "../assets/marklogosquare.png";
+interface Props {
+  src?: string;
+}
+
+withDefaults(defineProps<Props>(), {
+  src: logoImage,
+});
+</script>
+
+<style scoped>
+.responsive-img {
+  width: 80px;
+  height: 80px;
+  max-width: 100%;
+}
+</style>
