b12367d3cfa29e0d85567c5592a6f892fa7b93c0
Jajo1010 <ladislav.stefunjr@gmail.com>
2025-10-14 19:39:22 +0200

    feat: add logout

--- app/frontend/src/components/UI/AppDrawer.vue
+++ app/frontend/src/components/UI/AppDrawer.vue
@@ -1,19 +1,20 @@
 <template>
   <q-drawer v-model="drawer" show-if-above bordered>
-      <SearchBar />
-      <q-list bordered>
-        <q-scroll-area style="height:80vh;"  >
-          <DrawerItem name="Test1" iconName="contacts" is-new/>
-          <DrawerItem name="Ahoj" iconName="contacts"/>
-          <DrawerItem name="Ahoj" iconName="contacts"/>
+    <SearchBar />
+    <q-list bordered>
+      <q-scroll-area style="height: 80vh">
+        <DrawerItem name="Test1" iconName="contacts" is-new />
+        <DrawerItem name="Ahoj" iconName="contacts" />
+        <DrawerItem name="Ahoj" iconName="contacts" />
       </q-scroll-area>
-      </q-list>
-    </q-drawer>
+    </q-list>
+    <UserCard name="Milan" status="offline" />
+  </q-drawer>
 </template>
-
 
 <script setup lang="ts">
 import DrawerItem from "src/components/UI/DrawerItem.vue";
+import UserCard from "src/components/User/UserCard.vue";
 import { ref } from "vue";
 import SearchBar from "./SearchBar.vue";
 const drawer = ref(false);
