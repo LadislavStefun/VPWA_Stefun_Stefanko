c866c69220c72b22cc051f53df643a70d56b79e4
MartinStefanko1 <xstefanko@stuba.sk>
2025-10-14 17:59:27 +0200

    feat+fix:Added dd UI elements for managing channels + Fixed drawer button not working on tablet, mobile

--- app/frontend/src/components/UI/AppHeader.vue
+++ app/frontend/src/components/UI/AppHeader.vue
@@ -2,19 +2,20 @@
 <q-header elevated>
       <q-toolbar>
         <q-toolbar-title> {{ title }} </q-toolbar-title>
-        <q-btn flat @click="drawer = !drawer" round dense icon="menu" class="lt-md"/>
+        <q-btn flat   @click="$emit('toggle-drawer')" round dense icon="menu" class="lt-md"/>
       </q-toolbar>
 </q-header>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-const drawer = ref(false);
+
+
 
 interface Header {
   title:string
  }
 
 defineProps<Header>();
+defineEmits<{ 'toggle-drawer': [] }>()
 </script>
 
--- app/frontend/src/components/UI/DrawerItem.vue
+++ app/frontend/src/components/UI/DrawerItem.vue
@@ -9,19 +9,37 @@
           <q-item-section v-if="isNew" side >
           <q-badge text-color="white" label="New" />
           </q-item-section>
-        </q-item>
+
+        <q-btn  flat round dense icon="more_vert" @click.stop class="q-ml-xs"  >
+        <q-menu anchor="top right" self="top left">
+          <q-list style="min-width: 100px">
+            <q-item clickable v-close-popup>
+              <q-item-section>Delete channel</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>Leave channel</q-item-section>
+            </q-item>
+            <q-item clickable v-close-popup>
+              <q-item-section>List users</q-item-section>
+            </q-item>
+          </q-list>
+        </q-menu>
+      </q-btn>
+
+</q-item>
 
 </template>
 
 <script setup lang="ts">
+
  interface dItem {
   iconName: string
   name: string
   isNew?:boolean
  }
 
-
 withDefaults(defineProps<dItem>(), {
   isNew: false,
 });
+
 </script>
